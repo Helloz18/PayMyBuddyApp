6f227765de2147348fde33e6ea9bbebacf58aa62
Héloïse Finoux-Nikolic <hf2810@hotmail.com>
2020-12-11 12:32:48 +0100

    correction

--- src/main/java/com/PMB/PayMyBuddy/exception/NotEnoughMoneyException.java
+++ src/main/java/com/PMB/PayMyBuddy/exception/NotEnoughMoneyException.java
@@ -3,7 +3,7 @@ package com.PMB.PayMyBuddy.exception;
 public class NotEnoughMoneyException extends Exception {
 	
 	public NotEnoughMoneyException(String errorMessage) {
-		super(errorMessage);
+		super(errorMessage); 
 	}
 
 }
